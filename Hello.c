a683f8ef3d9d932dba7dd2168656203a65e1df32
Nikhil Tembhare <44189882+johnny190700@users.noreply.github.com>
2022-06-12 12:43:21 +0530

    Some Basic Program Added!
    Addition, Subtraction, Multiplication, Division, Modules and Hello World

---
+++ Hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <conio.h>
+int main(int argc, char const *argv[])
+{
+	clrscr();
+	printf("Hello, World!\n");
+	getch();
+	return 0;
+}
---
+++ Multi.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+int main(int argc, char const *argv[])
+{
+	int a,b,c;
+	clrscr();
+	printf("Enter Any Two Number\n");
+	scanf("%d%d",&a,&b);
+	c=a*b;
+	printf("Multiplication is %d\n",c);
+	getch();
+	return 0;
+}
